3314e862ab054552f225d8221c8b33b3011a412f
Kenikusss <208592207+Kenikusss@users.noreply.github.com>
2026-03-23 20:08:31 +0100

    Version Update

--- tss-website/eslint.config.mjs
+++ tss-website/eslint.config.mjs
@@ -1,14 +1,13 @@
 import { FlatCompat } from '@eslint/eslintrc'
- 
+
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
 })
- 
+
 const eslintConfig = [
   ...compat.config({
-    extends: ['next'],
-    plugins: ['import'],
+    extends: ['next/core-web-vitals'],
+    plugins: ['import', '@typescript-eslint'],
   }),
   {
     rules: {
@@ -29,5 +28,5 @@ const eslintConfig = [
     },
   },
 ]
- 
+
 export default eslintConfig
