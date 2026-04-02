5cc0255eb64591a1dc846b5ce7a1419383f3be7b
Kenikusss <208592207+Kenikusss@users.noreply.github.com>
2026-04-02 13:55:30 +0200

    Update page.tsx

--- tss-website/src/app/rejestracja/page.tsx
+++ tss-website/src/app/rejestracja/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { useLanguage } from "@/hooks/use-language";
-import registerUser from "./register-actions";
+import registerUser from "./actions";
 import { supabase } from "@/lib/supabase";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
