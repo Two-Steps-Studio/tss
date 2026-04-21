dc8cb953566b010e0bacfd3d3eca1b91a2646c96
Kenikusss <208592207+Kenikusss@users.noreply.github.com>
2026-04-21 21:56:16 +0200

    Bug Fix

--- tss-website/src/middleware.ts
+++ tss-website/src/middleware.ts
@@ -123,8 +123,9 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - api/ (API routes - handled separately if needed, or included)
+     * - manifest.webmanifest (PWA manifest)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|manifest.webmanifest).*)",
   ],
 };
